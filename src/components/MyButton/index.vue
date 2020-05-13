8ccb2153c602bd927f08d5e4307f02fdf0281ef1
YimWaiGwan <yanhuijun1024@163.com>
2020-05-13 21:03:12 +0800

    feat: add MyButton components

---
+++ src/components/MyButton/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-button">
+    <button>按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style lang='less' scoped>
+</style>
